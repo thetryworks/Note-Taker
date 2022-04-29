d7d6635f6bc181c8a278dd3bdf80bf67467a84e4
thetryworks <natesmyth1@gmail.com>
2022-04-29 13:29:22 -0500

    get request working

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/api/notes", (req, res) => {
     console.log("Here")
-    res.send("Hi")
-})
+    res.sendFile(path.join(__dirname, "/db/db.json" ))
+});
 
 
 
